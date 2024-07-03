6c7a3ff07f7ea22ccfa789d12227b8233fc3e7ed
Mohammed Hashim Beeran Kutty Kappat Nathirmuhsin <hashim_nathirmuhsin@stud.uni-bamberg.de>
2024-07-04 01:31:56 +0200

    updated and made some cosmetic changes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
+// Smooth scrolling for navigation links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
+    anchor.addEventListener('click', function(e) {
         e.preventDefault();
-
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
     });
+});
+
+// Show/hide navbar on scroll
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.querySelector("header").style.top = "0";
+    } else {
+        document.querySelector("header").style.top = "-80px";
+    }
+    prevScrollpos = currentScrollPos;
+};
+
+// Initialize AOS animations
+AOS.init({
+    duration: 1000,
+    easing: 'ease-in-out',
+    once: true,
+    mirror: false
 });
